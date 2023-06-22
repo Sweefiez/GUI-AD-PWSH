56606339f564ca14541a139483e9656073807103
Sweefiez <137406246+Sweefiez@users.noreply.github.com>
2023-06-22 15:37:48 +0200

    Add files via upload

---
+++ Function_github.ps1
@@ -0,0 +1,239 @@
+﻿# Program containing the functions necessary for the proper operation of the HMI for updating the AD
+
+# This function is used to update landline telephone numbers in the AD.
+
+function MAJFixes{
+    # these lines are used to search your computer files for the document containing the fixed numbers
+
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
+    $FileBrowser.Filter = "csv file (*.csv) |*.csv"
+    $FileBrowser.InitialDirectory = "C:\"
+    $FileBrowser.ShowDialog()
+
+    # These lines are used to retrieve the file name and path so that it can be processed in any 
+    # where the file is stored
+    $FixePath = $FileBrowser.FileName
+    $FixeFolder = ($FixePath.Split('\')[-1])
+    $FolderFile = $FixePath.Substring(0,($FixePath.Length-$FixeFolder.Length))
+    
+    # Here we check if the path is not null, if the path is null then we don't execute the program
+    if ($FixePath -notlike $null){
+
+        # Ici je sélectionne que les colonnes qui sont utiles aux traitement pour après et je les renomme pour faciliter 
+        #le traitement
+        Import-Csv -Path $FixePath -Delimiter ";" | 
+        Select-Object @{ expression={$_.'="Nom"'}; label="Nom"},
+        @{ expression={$_.'="Prénom"'}; label="Prenom"},
+        @{ expression={$_.'="numéro public"'}; label="numero_public" },
+        @{ expression={$_.'="numéro père"'}; label="numero_pere" },
+        @{ expression={$_.'="numéro interne"'}; label="numero_interne" } | 
+        Export-Csv -Path "$FolderFile\Temp_Result.CSV" -Delimiter ";"-Encoding UTF8 -NoTypeInformation
+
+        # Here I select only the columns that are necessary for the processing and rename them to facilitate the processing
+        Import-Csv -Path $FixePath -Delimiter ";" | 
+        Select-Object @{ expression={$_.'="Nom"'}; label="Nom"},
+        @{ expression={$_.'="Prénom"'}; label="Prenom"},
+        @{ expression={$_.'="numéro public"'}; label="numero_public" },
+        @{ expression={$_.'="numéro père"'}; label="numero_pere" },
+        @{ expression={$_.'="numéro interne"'}; label="numero_interne" } | 
+        Export-Csv -Path "$FolderFile\Temp_Result.CSV" -Delimiter ";" -Encoding UTF8 -NoTypeInformation
+
+        # Here I import the temporary CSV file created with only the necessary information
+        $newcsv = Import-Csv -Path "$FolderFile\Temp_Result.CSV" -Delimiter ";"
+        foreach($row in $newcsv){
+            # This loop removes unnecessary characters to make it more readable and easier to manipulate the data
+            $row.Nom = $row.Nom -replace '="','' -replace '"',''
+            $row.numero_interne = $row.numero_interne -replace '="','' -replace '"',''
+            $row.numero_pere = $row.numero_pere -replace '="','' -replace '"',''
+            $row.numero_public = $row.numero_public -replace '="','' -replace '"',''
+            $row.Prenom = $row.Prenom -replace '="','' -replace '"',''
+            # The line below adds a column named "nom_AD" to easily handle the information in AD processing
+            $row | Add-Member -MemberType NoteProperty -Name "nom_AD" -Value ($row.Prenom+" "+$row.Nom)
+            # This line takes the first letter of the first name and the full name as the SamAccountName in AD for easier processing later
+            $row.nom_AD = $row.nom_AD -replace '" "',' '
+        }
+
+        # Export the data with the new assignments made in the above loop
+        $newcsv | Export-Csv -Path "$FolderFile\Result.CSV" -Delimiter ";" -Encoding UTF8 -NoTypeInformation
+
+        # Remove the temporary file created above
+        Remove-Item -Path "$FolderFile\Temp_Result.CSV"
+        # Here we retrieve the processed file to perform the update in AD
+        $file_maj_fixes = Import-Csv -Path "$FolderFile\Result.CSV" -Delimiter ";"
+        # This loop takes each line from the file and checks if the username exists in AD, if it does, it checks if a primary number is specified in the corresponding column. If a primary number is specified, it adds it; otherwise, it adds the secondary number.
+        Foreach($User in $file_maj_fixes){
+            $Nom_AD = $User.nom_AD
+            if (Get-ADUser -Filter "Name -like ""$Nom_AD"""){
+                $num_pere = $User.numero_pere
+                $phone_public = $User.numero_public
+                if ($phone_public -like $null -and $num_pere -like $null){
+                    $ipPhone = $User.numero_interne
+                    $identity = (Get-ADUser -Filter "Name -like ""$Nom_AD""").SamAccountName
+                    Set-ADUser -Identity $identity -Replace @{ipPhone = $ipPhone}
+                    #Write-Host "Person : $Nom_AD, Tel ip : $ipPhone"
+                }
+                if ($phone_public -like $null -and $num_pere -notlike $null){
+                    $ipPhone = $User.numero_pere
+                    $identity = (Get-ADUser -Filter "Name -like ""$Nom_AD""").SamAccountName
+                    Set-ADUser -Identity $identity -Replace @{ipPhone = $ipPhone}
+                    #Write-Host "Person : $Nom_AD, Tel ip : $ipPhone"
+                }                                                                                                                                                                                          
+                if ($phone_public -notlike $null -and $num_pere -like $null){                                                                                                                                                           
+                    $OffPhone = $User.numero_public
+                    $ipPhone = $User.numero_interne
+                    $identity = (Get-ADUser -Filter "Name -like ""$Nom_AD""").SamAccountName
+                    Set-ADUser -Identity $identity -Replace @{ipPhone = $ipPhone} -OfficePhone $OffPhone
+                    #Write-Host "Person : $Nom_AD, Tel ip : $ipPhone, Office Phone : $OffPhone"
+                }
+                if($phone_public -notlike $null -and $num_pere -notlike $null){
+                    $OffPhone = $User.numero_public
+                    $ipPhone = $User.numero_pere
+                    $identity = (Get-ADUser -Filter "Name -like ""$Nom_AD""").SamAccountName
+                    Set-ADUser -Identity $identity -Replace @{ipPhone = $ipPhone} -OfficePhone $OffPhone
+                    #Write-Host "Person : $Nom_AD, Tel ip : $ipPhone, Office Phone : $OffPhone"
+                }
+            }
+        }
+    }
+        # Deletes the modified file so that there is no trace of it on the machine of the person executing the program.
+        Remove-Item -Path "$FolderFile\Result.CSV"
+}
+
+# This loop updates the mobile phone numbers in AD
+
+function MAJMobiles{
+    # This program selects a CSV file, extracts the necessary data, and saves it to a new CSV file.
+    # Then, the program uses the information from this new file to update the mobile phone information of users in the active directory.
+
+    # Creating a dialog window to select the CSV file
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
+    $FileBrowser.Filter = "csv file (*.csv) |*.csv"
+    $FileBrowser.InitialDirectory = "C:\"
+    $FileBrowser.ShowDialog()
+
+    # Storing the selected file path and name
+    $MobilesPath = $FileBrowser.FileName
+    $MobilesFolder = ($MobilesPath.Split('\')[-1])
+    $FolderFile = $MobilesPath.Substring(0,($MobilesPath.Length-$MobilesFolder.Length))
+
+    # Checking if the file was correctly selected
+    if ($MobilesPath -notlike $null){
+        # Extraction des données du fichier CSV sélectionné et stockage dans un nouveau fichier CSV
+        Import-Csv -Path $MobilesPath -Delimiter ";" -Encoding Default| `
+        Select-Object @{ expression={$_."nom utilisateur"}; label='nom_utilisateur' },@{ expression={$_."numéro"}; label='numero_mobile' } | Where-Object {$_ -notmatch ' - '} | `
+        Where-Object {$_ -notmatch 'BE'} | `
+        Export-Csv -Path "$FolderFile\Temp_Result_Tel_Mobiles.CSV" -Delimiter ";" -Encoding UTF8 -NoTypeInformation
+
+        # Loading the data from the new CSV file into memory
+        $ResultMobilesCSV = Import-csv -Path "$FolderFile\Temp_Result_Tel_Mobiles.CSV" -Delimiter ";" -Encoding Default
+
+        # Loop that reads each line of the imported CSV file
+        $i = 0
+        foreach ($row in $ResultMobilesCSV){
+            # The variable $name retrieves the username from each line of the file
+            $name = $ResultMobilesCSV.nom_utilisateur
+            # Splitting the name and checking its length, if it is greater than 2, then the condition is true and the code is executed
+            if ((($name[$i].split(" ")).Length -gt 2) -eq $true){
+                # Checking the number of uppercase words because all surnames are in uppercase.
+                # Here it checks if there are 3, 2, or 1 uppercase words in order to place the first name first followed by the last name
+                if (((($name[$i] -split " ") -cmatch '[A-Z]{2}').Length -eq 3) -eq $true){
+                    $row | Add-Member -MemberType NoteProperty -Name "nom_AD" -Value (($name[$i].split(" "))[-1]+" "+($name[$i].split(" "))[0]+" "+($name[$i].split(" "))[1]+" "+($name[$i].split(" "))[2]) -Force
+                }
+                if (((($name[$i] -split " ") -cmatch '[A-Z]{2}').Length -eq 2) -eq $true){
+                    $row | Add-Member -MemberType NoteProperty -Name "nom_AD" -Value (($name[$i].split(" "))[-1]+" "+($name[$i].split(" "))[0]+" "+($name[$i].split(" "))[1]) -Force
+                }
+                if (((($name[$i] -split " ") -cmatch '[A-Z]{2}').Length -eq 1) -eq $true){
+                    $row | Add-Member -MemberType NoteProperty -Name "nom_AD" -Value (($name[$i].split(" "))[1]+" "+($name[$i].split(" "))[-1]+" "+($name[$i].split(" "))[0]) -Force
+                }
+            }
+            # Reverse the first name and last name to have the first name first
+            if ((($name[$i].split(" ")).Length -eq 2) -eq $true){
+                $row | Add-Member -MemberType NoteProperty -Name "nom_AD" -Value (($name[$i].split(" "))[1]+" "+($name[$i].split(" "))[0]) -Force
+            }
+            if((($name[$i]).split(" ")).length -eq 1){
+                $row | Add-Member -MemberType NoteProperty -Name "nom_AD" -Value ($name[$i]) -Force
+            }
+            $i += 1
+        }
+
+        # This line saves the modified information into a CSV file named "result_PARC20122022.csv"
+        $ResultMobilesCSV | Export-csv "$FolderFile\Result_Tel_Mobiles.CSV" -Encoding UTF8 -Force -NoTypeInformation
+
+        # This line removes the temporary file created at the beginning of the script to avoid unnecessary files
+        Remove-item -Path "$FolderFile\Temp_Result_Tel_Mobiles.CSV"
+
+        # Import the previously processed file
+        $file_maj_mobiles = Import-Csv -Path "$FolderFile\Result_Tel_Mobiles.CSV" -Delimiter "," -Encoding Default
+        # The $Nom_AD variable retrieves the name from the "nom_ad" column, and the $Phone variable retrieves the phone number from the "numero_mobile" column
+        for($j=0;$j -lt ($file_maj_mobiles.Length);$j++){
+            $Nom_AD = ($file_maj_mobiles.nom_AD)[$j]
+            $Phone = ($file_maj_mobiles.numero_mobile)[$j]
+            # Check if the person exists in the AD, if so, execute the code
+            if (Get-ADUser -Filter "Name -like ""$Nom_AD"""){
+                # The $identity variable dynamically retrieves the user's identity, which consists of the first letter of the first name followed by the first 7 letters of the last name
+                $identity = (Get-ADUser -Filter "Name -like ""$Nom_AD""").SamAccountName
+                # Update the mobile phone field in the AD
+                Set-ADUser -Identity $identity -MobilePhone $Phone
+                #Write-Host "$Nom_AD -- $Phone" 
+            }     
+        }
+    }
+    # Delete the modified file to leave no trace on the machine of the person executing the program
+    Remove-item -Path "$FolderFile\Result_Tel_Mobiles.CSV"
+}
+
+function MAJEmployes{
+
+    # Create a window to choose a file
+    $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog
+    # Filter the displayed files to show only CSV files
+    $FileBrowser.Filter = "csv file (*.csv) |*.csv"
+    # Default directory when opening the selection window
+    $FileBrowser.InitialDirectory = "C:\"
+    # Show the window to choose a file
+    $FileBrowser.ShowDialog()
+
+    # Store the full path of the selected file
+    $EmploiPath = $FileBrowser.FileName
+
+    # If a file has been selected
+    if ($EmploiPath -notlike $null){
+        # Read the CSV file using a delimiter of ";"
+        $EmploiCSV = Import-csv -Path $EmploiPath -Delimiter ";" -Encoding Default
+        # For each row in the CSV file
+        foreach($row_emploi in $EmploiCSV){
+            # Combine the first name and last name of the person into a full name
+            $nom_AD = $row_emploi.Prénom +" "+ $row_emploi.Nom
+            # Store the person's job
+            $emploi = $row_emploi.Emploi
+            # Store the person's company
+            $societe = $row_emploi.Société
+            $Nsociete = $societe -replace ' S.A.S','' -replace 'SAS ',''
+            $Date_depart = $row_emploi.'Date départ'
+            $Date_today = Get-Date -Format 'dd/MM/yyyy'
+
+            # Search for the person in Active Directory using their full name
+            if (Get-ADUser -Filter "Name -like ""$nom_AD""")
+            {
+                if($Date_depart -like $null){
+                    # Store the username of the found person in Active Directory
+                    $identity = (Get-ADUser -Filter "Name -like ""$nom_AD""").SamAccountName
+                    # Update the person's job and description in Active Directory
+                    Set-ADUser -Identity $identity -Title $emploi -Description $emploi -Company $Nsociete
+                    #Write-Host "$nom_AD --- $emploi --- $Nsociete"
+                }
+                if($Date_depart -notlike $null){
+                    $Date_depart_obj = [datetime]::ParseExact($Date_depart, "dd/MM/yyyy", $null)
+                    $Date_today_obj = [datetime]::ParseExact($Date_today, "dd/MM/yyyy", $null)
+                    if($Date_depart_obj -ge $Date_today_obj){
+                        # Store the username of the found person in Active Directory
+                        $identity = (Get-ADUser -Filter "Name -like ""$nom_AD""").SamAccountName
+                        # Update position and person description in Active Directory
+                        Set-ADUser -Identity $identity -Title $emploi -Description $emploi -Company $Nsociete
+                        #Write-Host "$nom_AD --- $emploi --- $Nsociete"
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GUI_UPDATE_AD_github.ps1
@@ -0,0 +1,264 @@
+﻿Set-ExecutionPolicy -Force Unrestricted
+
+# This loop checks whether a session is open. As long as the session is not open, it continuously asks for the admin login and password to connect to a domain controller.
+while($dc.state -ne 'Opened'){
+
+# The lines below display a window to fill in a text field to enter your admin login 
+$form = New-Object System.Windows.Forms.Form
+$form.Size = New-Object System.Drawing.Size(200,150)
+$form.Text = "Login admin"
+
+# Create a text field
+$textBox = New-Object System.Windows.Forms.TextBox
+$textBox.Size = New-Object System.Drawing.Size(100,20)
+$textBox.Location = New-Object System.Drawing.Point(15,45)
+
+# Create a button
+$button_ok = New-Object System.Windows.Forms.Button
+$button_ok.AutoSize = $true
+$button_ok.Location = New-Object System.Drawing.Point(95,80)
+$button_ok.Text = "Valider"
+$button_ok.Add_Click({$form.Close()})
+
+# Create a label
+$label = New-Object System.Windows.Forms.Label
+$label.Location = New-Object System.Drawing.Size(10, 15)
+$label.AutoSize = $true
+$label.Text = "Entrez votre login d'admin :"
+
+# Adds the widgets defined above 
+$form.Controls.Add($textBox)
+$form.Controls.Add($label)
+$form.Controls.Add($button_ok)
+
+# Display window
+$form.ShowDialog() | Out-Null
+
+# The variable $login recovers the login written in the text field of the window created above, the variable $password displays a window to write the password securely.
+$login = $textBox.Text
+$password = Read-Host -Prompt "Entrez votre mot de passe" -AsSecureString
+# This variable $credential creates a login and password pair in order to connect to the domain controller.
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $login, $password
+
+# Define a list of machine names
+$machines = "DC01", "DC02", "DC03"
+
+# Initialize the variable that will store the name of the switched-on machine
+$poweredOnMachine = $null
+
+# For each machine in the list
+foreach ($machine in $machines) {
+    # Try to connect to the machine using WMI
+  try {
+    # If the connection is successful, the machine is switched on
+    if((Test-Connection $machine -Count 1 -Quiet) -eq "True"){
+        # Store the name of the machine switched on in the variable
+        $poweredOnMachine = $machine
+        # Exit the loop
+        break
+    }
+  } catch {
+    # If the connection fails, the machine is not switched on
+    # Go to the next machine in the list
+  }
+}
+
+# Allows you to create a session on the lit domain controller defined above, in order to import the AD module and make any necessary changes. 
+$dc = New-PSSession –ComputerName $poweredOnMachine -Credential $credential
+}
+# Imports the module named ActiveDirectory from the domain controller so that it can be updated in AD 
+Import-Module –PSSession $dc –Name ActiveDirectory -Force
+
+# Imports the modules contained in the Functions.ps1 file
+Import-Module -Name C:\0-projet_phpXpepsi\Liste_tel\Fonctions.ps1 -Force
+# Window creation
+$window = New-Object System.Windows.Forms.Form
+$window.Text = "Mise à jour de l'Active Directory"
+$window.Size = New-Object System.Drawing.Size(550,400)
+$window.StartPosition = "CenterScreen"
+$window.BackColor = [System.Drawing.Color]::FromArgb(0xEB, 0xEE, 0xEF)
+
+# Create a button with "Update fixed numbers" written inside
+$ButtonFixe = New-Object System.Windows.Forms.Button
+$ButtonFixe.Location = New-Object System.Drawing.Size(80,200)
+$ButtonFixe.Text = "Mise à jour des numéros fixes"
+$ButtonFixe.BackColor = [System.Drawing.Color]::FromArgb(0x26, 0x46, 0x91)
+$ButtonFixe.ForeColor = [System.Drawing.Color]::White
+$ButtonFixe.AutoSize = $true
+
+# Create a button with "Mobile number update" written inside
+$ButtonMobile = New-Object System.Windows.Forms.Button
+$ButtonMobile.Location = New-Object System.Drawing.Size(80,240)
+$ButtonMobile.Text = "Mise à jour des numéros mobiles"
+$ButtonMobile.BackColor = [System.Drawing.Color]::FromArgb(0x26, 0x46, 0x91)
+$ButtonMobile.ForeColor = [System.Drawing.Color]::White
+$ButtonMobile.AutoSize = $true
+
+# Create a button with "Function update" written inside
+$ButtonFonction = New-Object System.Windows.Forms.Button
+$ButtonFonction.Location = New-Object System.Drawing.Size(310,200)
+$ButtonFonction.Text = "Mise à jour de la fonction"
+$ButtonFonction.BackColor = [System.Drawing.Color]::FromArgb(0x26, 0x46, 0x91)
+$ButtonFonction.ForeColor = [System.Drawing.Color]::White
+$ButtonFonction.AutoSize = $true
+
+# Create a button with "Total update" written inside
+$ButtonComplet.Location = New-Object System.Drawing.Size(310,240)
+$ButtonComplet.Text = "Mise à jour complète"
+$ButtonComplet.BackColor = [System.Drawing.Color]::FromArgb(0x26, 0x46, 0x91)
+$ButtonComplet.ForeColor = [System.Drawing.Color]::White
+$ButtonComplet.AutoSize = $true
+
+
+# Creation of a button with "Validate" written inside
+$confirmButton = New-Object System.Windows.Forms.Button
+$confirmButton.Location = New-Object System.Drawing.Size(150,200)
+$confirmButton.Text = "Valider"
+$confirmButton.BackColor = [System.Drawing.Color]::FromArgb(0x26, 0x46, 0x91)
+$confirmButton.ForeColor = [System.Drawing.Color]::White
+$confirmButton.Visible = $false
+$confirmButton.AutoSize = $true
+
+
+# Creation of a button with "Back" written inside
+$backButton = New-Object System.Windows.Forms.Button
+$backButton.Location = New-Object System.Drawing.Size(250,200)
+$backButton.Text = "Retour"
+$backButton.BackColor = [System.Drawing.Color]::FromArgb(0x26, 0x46, 0x91)
+$backButton.Visible = $false
+$backButton.AutoSize = $true
+
+# Create a button with "Quit" written inside
+$quitButton = New-Object System.Windows.Forms.Button
+$quitButton.Location = New-Object System.Drawing.Size(225,300)
+$quitButton.Text = "Quitter"
+$quitButton.BackColor = [System.Drawing.Color]::FromArgb(0xBB, 0x00, 0x4D)
+$quitButton.ForeColor = [System.Drawing.Color]::White
+$quitButton.AutoSize = $true
+
+# Creating ToolTips for each button. ToolTips are small informative texts that appear when the mouse is held over the button for a few seconds.
+$MAJFixeToolTip = New-Object System.Windows.Forms.ToolTip
+$MAJFixeToolTip.SetToolTip($ButtonFixe, "Ce bouton permet de mettre à jour les téléphones fixe de chaque utilisateur de l'Active Directory")
+$MAJMobileToolTip = New-Object System.Windows.Forms.ToolTip
+$MAJMobileToolTip.SetToolTip($ButtonMobile, "Ce bouton permet de mettre à jour les téléphones mobiles de chaque utilisateur de l'Active Directory")
+$MAJFonctionToolTip = New-Object System.Windows.Forms.ToolTip
+$MAJFonctionToolTip.SetToolTip($ButtonFonction, "Ce bouton permet de mettre à jour les focntions de chaque utilisateur de l'Active Directory")
+$MAJCompleteToolTip = New-Object System.Windows.Forms.ToolTip
+$MAJCompleteToolTip.SetToolTip($ButtonComplet, "Ce bouton permet de mettre à jour tous les paramètres de chaque utilisateur de l'Active Directory")
+$MAJQuitToolTip = New-Object System.Windows.Forms.ToolTip
+$MAJQuitToolTip.SetToolTip($quitButton, "Ce bouton permet de quitter la fenêtre")
+
+# Add an image at the top of the window to make a banner
+$image1 = New-Object System.Windows.Forms.PictureBox
+$image1.Location = New-Object System.Drawing.Size(5,0)
+$image1.Size = New-Object System.Drawing.Size(520,210)
+$image1.Image = [System.Drawing.Image]::FromFile('C:\Images\Pictures.png')
+
+# Add the above defibi controls to the window
+$window.Controls.Add($ButtonFixe)
+$window.Controls.Add($ButtonMobile)
+$window.Controls.Add($ButtonFonction)
+$window.Controls.Add($ButtonComplet)
+$window.Controls.Add($confirmButton)
+$window.Controls.Add($backButton)
+$window.Controls.Add($quitButton)
+$window.Controls.Add($image1)
+
+
+# Event management
+$ButtonFixe.Add_Click({
+#Displays a confirmation message and shows confirmation and back buttons
+
+$messageBox = [System.Windows.Forms.MessageBox]::Show("Êtes-vous sûr de vouloir mettre à jour les numéros de téléphones fixes ?", "Confirmation", [System.Windows.Forms.MessageBoxButtons]::YesNo)
+if ($messageBox -eq "Yes") {
+# Executes the MAJFixes function created in the "Functions" file
+MAJFixes
+Write-Host "Les téléphones fixes sont mis à  jours"
+Write-Host "-------------------------------------------------"
+[System.Windows.MessageBox]::Show("Opération réalisée avec succès !")
+}
+$confirmButton.Visible = $false
+$backButton.Visible = $false
+})
+
+$ButtonMobile.Add_Click({
+#Displays a confirmation message and shows confirmation and back buttons
+
+$messageBox = [System.Windows.Forms.MessageBox]::Show("Êtes-vous sûr de vouloir mettre à jour les numéros de téléphones mobiles ?", "Confirmation", [System.Windows.Forms.MessageBoxButtons]::YesNo)
+if ($messageBox -eq "Yes") {
+# Executes the MAJMobiles function created in the "Functions" file
+MAJMobiles
+Write-Host "Les téléphones mobiles sont mis à  jours"
+Write-Host "-------------------------------------------------"
+[System.Windows.MessageBox]::Show("Opération réalisée avec succès !")
+}
+$confirmButton.Visible = $false
+$backButton.Visible = $false
+})
+
+$ButtonFonction.Add_Click({
+#Displays a confirmation message and shows confirmation and back buttons
+
+$messageBox = [System.Windows.Forms.MessageBox]::Show("Êtes-vous sûr de vouloir mettre à jour les fonctions des employés ?", "Confirmation", [System.Windows.Forms.MessageBoxButtons]::YesNo)
+if ($messageBox -eq "Yes") {
+# Executes the MAJEmployes function created in the "Functions" file
+MAJEmployes
+Write-Host "Les fonctions des employés sont mise à  jours"
+Write-Host "-------------------------------------------------"
+[System.Windows.MessageBox]::Show("Opération réalisée avec succès !")
+}
+$confirmButton.Visible = $false
+$backButton.Visible = $false
+})
+
+$ButtonComplet.Add_Click({
+#Displays a confirmation message and shows confirmation and back buttons
+
+$messageBox = [System.Windows.Forms.MessageBox]::Show("Êtes-vous sûr de vouloir faire une mise à jour complète ?", "Confirmation",[System.Windows.Forms.MessageBoxButtons]::YesNo)
+if ($messageBox -eq "Yes") {
+# Execute the MAJFixes, MAJMobiles and MAJEmployes functions created in the "Functions" file, asking for the right file in a pop-up window at the start of each function execution.
+[System.Windows.MessageBox]::Show("Choisissez le ficheir Mitel svp")
+MAJFixes
+[System.Windows.MessageBox]::Show("Choisissez le ficheir Orange svp")
+MAJMobiles
+[System.Windows.MessageBox]::Show("Choisissez le ficheir RH svp")
+MAJEmployes
+
+Write-Host "-------------------------------------------------"
+[System.Windows.MessageBox]::Show("Opération réalisée avec succès !")
+}
+$confirmButton.Visible = $false
+$backButton.Visible = $false
+})
+
+$confirmButton.Add_Click({
+# Executes the action associated with the selected button
+
+Write-Host "Action associée au bouton sélectionné"
+$confirmButton.Visible = $false
+$backButton.Visible = $false
+})
+
+$backButton.Add_Click({
+#Return to selection window
+
+$confirmButton.Visible = $false
+$backButton.Visible = $false
+})
+
+$quitButton.Add_Click({
+# Close the window
+$messageBox = [System.Windows.Forms.MessageBox]::Show("Êtes-vous sûr de vouloir quitter la fenêtre ?", "Confirmation", [System.Windows.Forms.MessageBoxButtons]::YesNo)
+if ($messageBox -eq "Yes") {
+# Executes the Close action to close the window
+$window.Close()
+}
+$confirmButton.Visible = $false
+$backButton.Visible = $false
+})
+# Displays the window
+
+$window.ShowDialog()
+
+# Disconnect the session created on the domain controller
+Remove-PSSession -Session $dc
